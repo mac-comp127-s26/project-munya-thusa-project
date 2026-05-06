936150311aa466160923c8fb0269a2c32425dccd
2saB <pthusaba@macalester.edu>
2026-05-05 21:49:39 -0500

    update
    added some eyes and a tongue to the snake and got rid of the grid lines in the snake

--- src/Snake.java
+++ src/Snake.java
@@ -23,6 +23,11 @@ public class Snake {
     private static final int SCREEN_WIDTH = 800;
     private static final int SCREEN_HEIGHT = 600;
 
+    private Rectangle headVisual;
+    private Rectangle leftEye;
+    private Rectangle rightEye;
+    private Rectangle tongue;
+
     public Snake(GraphicsGroup layer) {
 
         this.layer = layer;
@@ -32,10 +37,12 @@ public class Snake {
         for (int i = 0; i < 5; i++) {
             Rectangle part = new Rectangle(100 - i * SIZE, 100, SIZE, SIZE);
             part.setFillColor(Color.GREEN);
+            part.setStrokeColor(Color.GREEN);
 
             body.add(part);
             layer.add(part);
         }
+        createHead();
     }
 
 
@@ -101,6 +108,7 @@ public class Snake {
                 return false; // hit itself → game over
             }
         }
+        updateHeadPosition();
         return true;
     }
 
@@ -132,8 +140,82 @@ public class Snake {
             SIZE);
 
         newPart.setFillColor(Color.GREEN);
+        newPart.setStrokeColor(Color.GREEN);
 
         body.add(newPart);
         layer.add(newPart);
+    }
+
+    private void createHead() {
+
+        Rectangle realHead = body.get(0);
+
+        // Head
+        headVisual = new Rectangle(
+            realHead.getX(),
+            realHead.getY(),
+            SIZE,
+            SIZE);
+        headVisual.setFillColor(new Color(0, 180, 0));
+        headVisual.setStrokeColor(new Color(0, 180, 0));
+
+        // Eyes
+        leftEye = new Rectangle(0, 0, 4, 4);
+        rightEye = new Rectangle(0, 0, 4, 4);
+
+        leftEye.setFillColor(Color.WHITE);
+        rightEye.setFillColor(Color.WHITE);
+
+        // Tongue
+        tongue = new Rectangle(0, 0, 6, 2);
+        tongue.setFillColor(Color.RED);
+
+        layer.add(headVisual);
+        layer.add(leftEye);
+        layer.add(rightEye);
+        layer.add(tongue);
+
+        updateHeadPosition();
+    }
+
+    private void updateHeadPosition() {
+
+        Rectangle realHead = body.get(0);
+
+        double x = realHead.getX();
+        double y = realHead.getY();
+
+        headVisual.setPosition(x, y);
+
+        // Position eyes + tongue based on direction
+        if (direction == 'R') {
+
+            leftEye.setPosition(x + 12, y + 4);
+            rightEye.setPosition(x + 12, y + 12);
+
+            tongue.setPosition(x + SIZE, y + SIZE / 2);
+
+        } else if (direction == 'L') {
+
+            leftEye.setPosition(x + 4, y + 4);
+            rightEye.setPosition(x + 4, y + 12);
+
+            tongue.setPosition(x - 6, y + SIZE / 2);
+
+        } else if (direction == 'U') {
+
+            leftEye.setPosition(x + 4, y + 4);
+            rightEye.setPosition(x + 12, y + 4);
+
+            tongue.setPosition(x + SIZE / 2, y - 6);
+
+        } else if (direction == 'D') {
+
+            leftEye.setPosition(x + 4, y + 12);
+            rightEye.setPosition(x + 12, y + 12);
+
+            tongue.setPosition(x + SIZE / 2, y + SIZE);
+
+        }
     }
 }
